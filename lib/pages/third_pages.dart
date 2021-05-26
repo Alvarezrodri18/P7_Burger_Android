a14c0db61f610db399de528bbc91e8485babd0aa
FlutLab.io <>
2021-05-26 17:36:09 +0000

    Autosave

--- lib/pages/third_pages.dart
+++ lib/pages/third_pages.dart
@@ -3,20 +3,6 @@ import 'package:flutter/material.dart';
 class ThirdPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Third Page',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
-        ), //Text
-      ), // center
-    ); //container
-  } // widget
-} // Third page
-
-class ThirdPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cards'),
