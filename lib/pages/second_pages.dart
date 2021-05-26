a801ead8951abb18baedf957a66b25bb291ab8e9
FlutLab.io <>
2021-05-26 17:23:34 +0000

    Autosave

--- lib/pages/second_pages.dart
+++ lib/pages/second_pages.dart
@@ -1,15 +1,79 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulario'),
+        flexibleSpace: Container ( 
+              decoration: BoxDecoration ( 
+                gradient: LinearGradient ( 
+                  begin: Alignment.bottomRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.cyan, Colors.blue, Colors.indigo], 
+                ), 
+              ), 
+            ), 
+      ),//AppBar
+      body: SingleChildScrollView(
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              _crearInput1(),
+              SizedBox(height: 20,),
+                _crearInput2(),
+                   SizedBox(height: 20,),
+                _crearInput3(),
+                FlatButton(
+                  onPressed: () {},
+                  color: Colors.indigo,
+                  child: Text('Enviar', style: TextStyle(color: Colors.white),),
+                )
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+    
+  }
+  Widget _crearInput1() {
     return Container(
-      child: Center(
-        child: Text(
-          'Second Page',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
-        ), //Text
-      ), // center
-    ); //container
-  } // widget
-} // Second page
+      padding: EdgeInsets.all(15),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Nombre Completo',
+          hintText: 'Nombre'
+        ),
+      ),
+    );
+  }
+   Widget _crearInput3() {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: TextFormField(
+        
+        decoration: InputDecoration(
+          labelText: 'Email',
+          icon: Icon(Icons.email),
+          hintText: 'Email'
+        ),
+      ),
+    );
+  }
+  Widget _crearInput2() {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          hintText: 'Password'
+        ),
+      ),
+    );
+  }
+}// Second page
