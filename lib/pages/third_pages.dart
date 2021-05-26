6124b9cb30072be5dc1554e58b619bc30edd655c
FlutLab.io <>
2021-05-26 17:35:53 +0000

    Autosave

--- lib/pages/third_pages.dart
+++ lib/pages/third_pages.dart
@@ -13,3 +13,85 @@ class ThirdPage extends StatelessWidget {
     ); //container
   } // widget
 } // Third page
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              _crearCard1(),
+              _crearCard2()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearCard2() {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Container(
+        decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.orange[400],
+                Colors.deepOrange[900],
+              ],
+            )
+            ),
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Consumo NVIDIA',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Según NVIDIA esta tarjeta gráfica tiene un consumo medio de 320 vatios, por lo que recomienda instalarla en un PC que tenga una fuente de alimentación con una capacidad de entrega de potencia teórica de al menos 750 vatios.',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearCard1() {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/burger.gif'),
+                image: NetworkImage('https://hipertextual.com/files/2019/09/hipertextual-the-legend-of-zelda-links-awakening-2019999870.jpg'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Soy una card con imagen'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}//ThirdPage
